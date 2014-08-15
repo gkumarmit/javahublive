4765567e98a88c5b20cb51dacbd9fa47fabda6c1
Ganesh Kumar <gkumarmit@gmail.com>
2014-08-15 23:36:00 +0530

    Spring Data Project Example
    It explains to use of spring data project using jpa in application.

---
+++ springdata/src/main/java/com/javahublive/springdata/dao/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright @2014 By JavaHubLive.com
+ *
+ * All rights reserved. The source code is proprietary of JavaHubLive.com. 
+ * The source code may not be reproduced or modify to any persons or third party entities
+ * without prior notice to JavaHubLive.com.
+ *
+ * Created on 15-Aug-2014
+ *
+ * Last edited by:      $Author: $
+ *             on:      $Date: $
+ *       Filename:      $Id: $
+ * 
+ */
+
+/**
+ * @author ganesh
+ *
+ */
+package com.javahublive.springdata.dao;
---
+++ springdata/src/main/java/com/javahublive/springdata/domain/Person.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright @2014 By JavaHubLive.com
+ *
+ * All rights reserved. The source code is proprietary of JavaHubLive.com. 
+ * The source code may not be reproduced or modify to any persons or third party entities
+ * without prior notice to JavaHubLive.com.
+ *
+ * Created on 15-Aug-2014
+ *
+ * Last edited by:      $Author: $
+ *             on:      $Date: $
+ *       Filename:      $Id: $
+ * 
+ */
+
+package com.javahublive.springdata.domain;
+
+/**
+ * @author ganesh
+ *
+ */
+public class Person {
+
+	private Long personId;
+	private String firstName;
+	private String lastName;
+	private String address;
+
+	/**
+	 * @return the personId
+	 */
+	public Long getPersonId() {
+		return personId;
+	}
+
+	/**
+	 * @param personId
+	 *            the personId to set
+	 */
+	public void setPersonId(Long personId) {
+		this.personId = personId;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName
+	 *            the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName
+	 *            the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address
+	 *            the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result
+				+ ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result
+				+ ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result
+				+ ((personId == null) ? 0 : personId.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Person)) {
+			return false;
+		}
+		Person other = (Person) obj;
+		if (address == null) {
+			if (other.address != null) {
+				return false;
+			}
+		} else if (!address.equals(other.address)) {
+			return false;
+		}
+		if (firstName == null) {
+			if (other.firstName != null) {
+				return false;
+			}
+		} else if (!firstName.equals(other.firstName)) {
+			return false;
+		}
+		if (lastName == null) {
+			if (other.lastName != null) {
+				return false;
+			}
+		} else if (!lastName.equals(other.lastName)) {
+			return false;
+		}
+		if (personId == null) {
+			if (other.personId != null) {
+				return false;
+			}
+		} else if (!personId.equals(other.personId)) {
+			return false;
+		}
+		return true;
+	}
+
+}
